5f317628ef8ad450857a82445603fea4617ec214
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2024-03-30 19:08:03 -0300

    Footer creation

---
+++ Acc-challenge/src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+
+  return (
+    <div className='container-footer' id='footer'>
+        <h1>Gracias por <span className='secondary-color'>completar el ejercicio</span></h1>
+        <h2>Te invitamos a ver mas informacion.</h2>
+        <button className='btn'>Conocer mas</button>
+    </div>
+  );
+};
+
+export default Footer;
